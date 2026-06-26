f26bd6cc2ea1d2095fdc21253940bcae2a672b5e
StevenArmijo <stevenarmijo11@gmail.com>
2026-06-26 14:21:29 -0700

    Made Client Feedback Active with JS

--- script.js
+++ script.js
@@ -28,3 +28,96 @@ closeModalBtns.forEach((btn) => {
     modal.classList.remove("active");
   });
 });
+
+// CAROUSEL SECTION IN SERVICES 
+const reviews = [
+  {
+    quote: "Nick made the entire process seamless.",
+    name: "Jessica M.",
+    location: "Lancaster"
+  },
+  {
+    quote: "Nick is my closest friend we do everything together",
+    name: "Hugh Janus",
+    location: "Palmdale"
+  },
+  {
+    quote: "Nick was the nicest person to mee but out of nowhere i caught him flirting with another girl in front of me",
+    name: "Ashley",
+    location: "Anaheim"
+  },
+  {
+    quote: "nick is gay",
+    name: "steven",
+    location: "Palmdale"
+  }
+];
+const feedbackQuote = document.querySelector(".feedbackQuote");
+const feedbackName = document.querySelector(".feedbackName");
+const feedbackDots = document.querySelector(".feedbackDots");
+
+let currentReviewIndex = 0;
+let reviewTimer;
+const reviewDuration = 5000;
+
+function showReview(index) {
+  const review = reviews[index];
+
+  feedbackQuote.textContent = review.quote;
+  feedbackName.textContent = `${review.name}, ${review.location}`;
+
+  currentReviewIndex = index;
+
+const dots = document.querySelectorAll(".feedbackDot");
+
+  dots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+
+  if (dots[index]) {
+    dots[index].classList.add("active");
+  }
+
+  currentReviewIndex = index;
+}
+
+
+function showNextReview(){
+   currentReviewIndex++;
+
+   if (currentReviewIndex >= reviews.length) {
+    currentReviewIndex = 0;
+   }
+   showReview(currentReviewIndex)
+}
+
+  function createDots() {
+    reviews.forEach((review, index) => {
+      const dot = document.createElement("button");
+
+      dot.classList.add("feedbackDot");
+      dot.type = "button";
+
+      dot.addEventListener("click", () => {
+        showReview(index);
+        resetTimer();
+      });
+      feedbackDots.appendChild(dot);
+    });
+  };
+
+  function startTimer () {
+    reviewTimer = setInterval(showNextReview, reviewDuration);
+  }
+
+function resetTimer () {
+  clearInterval(reviewTimer);
+  startTimer();
+}
+
+if (feedbackQuote && feedbackName && feedbackDots) {
+  console.log("Review carousel started");
+  createDots();
+  showReview(0);
+  startTimer();
+}
